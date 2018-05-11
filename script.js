de89e94ac031ad8ffcd7969971c632e306f2234b
Trevor Fitzgerald <fitztrev@gmail.com>
2018-05-11 09:51:54 -0400

    updates

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 new Vue({
     el: '#app',
 
-    //https://vuejs.org/v2/guide/components.html#data-Must-Be-a-Function
     data() {
         return {
-            holes: Array.from({length: 18}, null),
+            holes: Array.from({ length: 18 }, () => null),
+
             holeWithFocus: 0,
 
             numberpad: [
@@ -21,7 +21,7 @@ new Vue({
 
     computed: {
         holesFront() {
-            return this.holes.slice(0, 8)
+            return this.holes.slice(0, 9)
         },
 
         holesBack() {
@@ -43,25 +43,26 @@ new Vue({
 
     methods: {
         clearScores() {
-            this.holes = Array.from({length: 18}, null)
+            this.holes = Array.from({ length: 18 }, () => null)
+
             this.holeWithFocus = 0
         },
 
         setHoleScore(score) {
             if (this.holeWithFocus > 17) return
 
-            this.holes[this.holeWithFocus] = score
+            this.$set(this.holes, this.holeWithFocus, score)
+
             this.holeWithFocus++
         },
 
         registerKeyboardListener() {
-            // this seems a little wrong, we should probably scope this to this.$el.addEventListener
-            // but given I don't know your template I can't assume that
-            window.addEventListener("keypress", (e) => {
-                if(e.keyCode >= 48 && e.keyCode <= 57) {
-                    this.setHoleScore(pressed);
+            window.addEventListener('keypress', e => {
+                let key = e.keyCode
+                if (key >= 48 && key <= 57) {
+                    this.setHoleScore(key - 48)
                 }
             })
-        }
+        },
     },
 })
